c32cb21563f7bdc6f64d3b195a17a5d3e0220d50
ramveermeena2003 <ramveermeena2003@gmail.com>
2025-06-10 17:38:57 +0530

    commit after frontend setup

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 # Add  "rejected" and "cancelled" to the enum for full control [friendRequest.js]
 
 # Add unique index on [sender, recipient] to prevent duplicate requests [friendRequest.js]
+
+# Website - storyset.com
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import {Routes, Route, Navigate } from "react-router"
+import Homepage from './pages/Homepage.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import NotificationsPage from './pages/NotificationsPage.jsx'
+import CallPage from './pages/CallPage.jsx'
+import ChatPage from './pages/ChatPage.jsx'
+import OnboardingPage from './pages/OnboardingPage.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
+import {Toaster} from "react-hot-toast";
+import { useQuery } from '@tanstack/react-query'
+import axios from "axios";
+import { axiosInstance } from './lib/axios.js'
+
+const App = () => {
+
+  //tanstack query
+
+  const {data:authData,isLoading, error} = useQuery({
+    queryKey : ["authUser"],
+    queryFn : async () => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+    retry:false,
+  });
+
+  const authUser = authData?.user;
+
+  console.log(authUser);
+  console.log(authData);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='h-screen'>
+      
+      <Routes>
+        <Route path='/' element={authUser ? <Homepage/> : <Navigate to="/login"/>}/>
+        <Route path='/signup' element={!authUser ? <SignUpPage/> : <Navigate to="/"/>}/>
+        <Route path='/login' element={!authUser ? <LoginPage/> : <Navigate to="/"/>}/>
+        <Route path='/notifications' element={authUser ? <NotificationsPage/> : <Navigate to="/login"/>}/>
+        <Route path='/call' element={authUser ? <CallPage/> : <Navigate to= "/login"/>}/>
+        <Route path='/chat' element={authUser ? <ChatPage/> : <Navigate to="/login"/>}/>
+        <Route path='/onboarding' element={authUser ? <OnboardingPage/> : <Navigate to="/login"/>}/>
+      </Routes>
+
+      <Toaster/>
+    </div>
   )
 }
 
